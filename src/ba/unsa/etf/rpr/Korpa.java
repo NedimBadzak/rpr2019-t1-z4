7253ca701259d5ad5d8ffa23a87ee5907caca845
Nedim Badzak <nbadzak1@etf.unsa.ba>
2020-10-24 17:35:31 +0200

    Implementirane klase Artikl, Korpa i Supermarket

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,54 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = null;
+    private final int max = 50;
+    private int velicinaKorpe = 0;
+
+    public Korpa() {
+        artikli = new Artikl[max];
+    }
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getVelicinaKorpe() {
+        return velicinaKorpe;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        if(getVelicinaKorpe() < max) {
+            artikli[getVelicinaKorpe()] = new Artikl(a);
+            velicinaKorpe++;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl temp = null;
+        for (int i = 0; i < getVelicinaKorpe(); i++) {
+            if (this.artikli[i].getKod().equals(kod)) {
+                temp = artikli[i];
+                artikli[i] = null;
+            }
+        }
+        return temp;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int zbir = 0;
+        for (int i = 0; i < getVelicinaKorpe(); i++) {
+            if(artikli[i] != null) zbir = zbir + artikli[i].getCijena();
+        }
+        return zbir;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,44 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = null;
+    private final int max = 1000;
+    private static int brojArtikala = 0;
+
+    public Supermarket() {
+        this.artikli = new Artikl[max];
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public static int getBrojArtikala() {
+        return brojArtikala;
+    }
+
+    public void dodajArtikl(Artikl a) {
+        artikli[getBrojArtikala()] = new Artikl(a);
+        brojArtikala++;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl temp = null;
+        for (int i = 0; i < getBrojArtikala(); i++) {
+            if (artikli[i].getKod().equals(kod)) {
+                temp = artikli[i];
+                artikli[i] = null;
+                break;
+            }
+        }
+        return temp;
+    }
 }
